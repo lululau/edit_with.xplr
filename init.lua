46f5ec8e1b4e1343853fbb5273f7fb48df9dfad1
Liu Xiang <liuxiang@ktjr.com>
2023-08-30 16:44:28 +0800

    Initial commit

---
+++ init.lua
@@ -0,0 +1,57 @@
+local q = xplr.util.shell_quote
+
+local function setup()
+  local xplr = xplr
+
+  xplr.config.modes.custom.edit_with = {
+    name = "edit focused file with nvim/emacs/vscode editor",
+    key_bindings = {
+      on_key = {
+        "c" = {
+          help = "edit with vscode",
+          messages = {
+            "PopMode",
+            {
+              BashExecSilently0 = [===[
+                code "${XPLR_FOCUS_PATH:?}"
+                ]===]
+            }
+          },
+        },
+      },
+
+      "e" = {
+        help = "edit with emacs",
+        messages = {
+          "PopMode",
+          {
+            BashExecSilently0 = [===[
+              emacsclient "${XPLR_FOCUS_PATH:?}"
+              ]===]
+          }
+        },
+      },
+    },
+
+    "v" = {
+      help = "edit with nvim",
+      messages = {
+        "PopMode",
+        {
+          BashExec0 = [===[
+            nvim "${XPLR_FOCUS_PATH:?}"
+            ]===]
+        }
+      },
+    },
+  }
+
+  xplr.config.modes.builtin.default.key_bindings.on_key["e"] = {
+    help = "",
+    messages = {
+      { SwitchModeCustom = "edit_with" },
+    },
+  }
+end
+
+return { setup = setup }
